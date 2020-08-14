8ad16b402f36def63f8f6f593b068d77b1339182
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-14 11:25:32 -0600

    Initial commit

---
+++ src/endUp.java
@@ -0,0 +1,12 @@
+public class endUp {
+
+  public String endUp(String str) {
+    if (str.length() < 3) {
+      return str.toUpperCase();
+    } else {
+      return (str.substring(0, str.length() - 3)
+          + str.substring(str.length() - 3, str.length()).toUpperCase());
+    }
+  }
+
+}
